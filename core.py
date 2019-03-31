9254221e3e16085b845c349e143df18e006c2dce
ZestyPepper <47640485+ZestyPepper@users.noreply.github.com>
2019-03-31 10:23:23 -0600

    Add files via upload

---
+++ core.py
@@ -0,0 +1,28 @@
+print()
+print("================")
+print()
+print("Welcome...")
+print()
+print("This is an encryption and decryption software...")
+primt()
+print("We use magic to encrypt our messages...")
+print()
+
+userInput = input(str("Please enter a message to encrypt: "))
+
+encryptedMSG = ''
+
+for letter in userInput:
+    encryptedMSG += chr(ord(letter) + 5)
+
+print(encryptedMSG)
+
+print()
+userInput = input(str("Enter the message you want decrypted: "))
+
+decryptedMSG = ' '
+
+for letter in userInput:
+    decryptedMSG += chr(ord(letter) - 5)
+
+print("Your decrypted message is:" + (decryptedMSG))
